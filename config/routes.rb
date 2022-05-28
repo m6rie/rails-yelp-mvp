dff1ab27bf3b09c28771f119131aec3235f027ce
Marie Etong <m6rief@gmail.com>
2022-05-28 20:00:07 +0100

    yelp challenge

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # index, new, create, edit, update, show, destroy
+  # for restaurants: index, new, create, show + reviews/show
+  # for review: new, create
+
+  root to: 'restaurants#index'
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:index, :create]
+  end
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:restaurant_id', to: 'restaurants#show', as: :restaurant
+  # # get 'restaurants/:id/reviews', to: 'reviews#index'
+
+  # get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_review
+  # post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
+
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+river_view = { name: 'River View', address: 'New Crane Wharf, London E1W 3TU', category: 'chinese', phone_number: '+321 23 45 67 89' }
+
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian', phone_number: '+331 23 45 67 89' }
+
+dotori = { name: 'Dotori Japanese Sushi', address: '3 Stroud Green Rd, London N4 2DQ', category: 'japanese', phone_number: '+491 23 45 67 89' }
+
+mukkebi = { name: 'Mukkebi', address: '152A Seven Sisters Rd, London N7 7PL', category: 'japanese', phone_number: '+311 23 45 67 89' }
+
+casse_croute = { name: 'Casse-Croûte', address: '109 Bermondsey St, London SE1 3XB', category: 'french', phone_number: '+441 23 45 67 89' }
+
+auberge = { name: 'Auberge', address: '1 Sandell St, London SE1 8UH', category: 'french' }
+
+[river_view, pizza_east, dotori, mukkebi, casse_croute, auberge].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
